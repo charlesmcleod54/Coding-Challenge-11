3aed558b827b7cf37ac9de97d2591a2a503cee1d
Charles McLeod <charlesmcleod@usf.edu>
2025-03-02 18:25:58 -0500

    Task 1 - Created Book Class

---
+++ cc_11.js
@@ -0,0 +1,23 @@
+// Task 1: Creating a Book Class
+class Book {
+    constructor(title, author, isbn, copies) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.copies = copies;
+    }
+
+    getDetails() {
+        return 'Title: ${this.title}, Author: ${this.author}, ISBN: ${this.isbn}, Copies: {this.copies}';
+    }
+
+    updateCopies(quantity) {
+        this.copies += quantity;
+    }
+}
+
+const book1 = new Book("Frankenstein", "Mary Shelley", 274958, 10);
+
+book1.updateCopies(-1);
+console.log(book1.getDetails());
+
